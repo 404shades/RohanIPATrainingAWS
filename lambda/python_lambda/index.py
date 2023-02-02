19e46837dc263d9b8eaf0221828d18f084e310f7
Rohan Malik <malikmalik29@outlook.com>
2023-02-02 13:59:29 +0530

    added destination bucket to copy object from source

--- lambda/python_lambda/index.py
+++ lambda/python_lambda/index.py
@@ -1,3 +1,12 @@
+import boto3
+
+
+def get_s3_client():
+    return boto3.client('s3')
+
+
 def handler(event, context):
+    source_bucket_name = event["Records"][0]['s3']['bucket']['name']
+    print(source_bucket_name)
     print(event)
     return "'Hello Lambda'"
