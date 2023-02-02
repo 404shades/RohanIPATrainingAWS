8a663963829292c3eadd0ef27d6b1c111ba98242
Rohan Malik <malikmalik29@outlook.com>
2023-02-02 15:03:59 +0530

    added linter stage to validate cloud formation template

---
+++ README.md
@@ -0,0 +1,16 @@
+![cf workflow](https://github.com/404shades/RohanIPATrainingAWS/actions/workflows/deploy-ci-cd.yml/badge.svg)
+
+# Week1
+- Create Cloud Formation Template for event based trigger on s3 using lambda function. Whenever a new object is inserted in s3 bucket it should trigger the lambda and copy the newly inserted object to another bucket as backup
+
+## Resources Created
+- Lambda Function
+- Source Bucket
+- Destination
+- IAM Role
+- Notification Trigger on S3 Bucket
+
+## CI/CD Stages
+
+
+
--- lambda/python_lambda/index.py
+++ lambda/python_lambda/index.py
@@ -4,15 +4,31 @@ import os
 
 
 def get_s3_client():
+    """
+    Get S3 Client from boto3
+    :return:
+    """
     return boto3.client('s3')
 
 
 def copy_file_to_destination(copy_object, file_name):
+    """
+    Copies file from source bucket to destination bucket with given file name(key)
+    :param copy_object: {Bucket:"SOURCE BUCKET NAME", Key: "File to copy"
+    :param file_name: File name to be used after copying in destination
+    :return:
+    """
     destination_bucket_name = os.environ["DESTINATION_BUCKET"]
     return get_s3_client().copy_object(CopySource=copy_object, Bucket=destination_bucket_name, Key=file_name)
 
 
 def handler(event, context):
+    """
+    Lambda handler to be called on lambda function trigger
+    :param event:
+    :param context:
+    :return:
+    """
     source_bucket_name = event["Records"][0]['s3']['bucket']['name']
     source_object_key = event["Records"][0]['s3']['object']['key']
     copy_object = {'Bucket': source_bucket_name, 'Key': source_object_key}
